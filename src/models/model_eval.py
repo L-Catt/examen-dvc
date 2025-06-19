f55c28c775e162a993d65688f1f624792714bcdb
L Catt <cattoire.jobs@gmail.com>
2025-06-19 15:00:58 +0200

    add metrics results

---
+++ src/models/model_eval.py
@@ -0,0 +1,25 @@
+#evaluation du modèle
+import pandas as pd
+from sklearn.metrics import mean_squared_error, r2_score
+import joblib
+# Chargement des données prétraitées
+X_test = pd.read_csv('data/processed_data/X_test_scaled.csv')
+y_test = pd.read_csv('data/processed_data/y_test.csv')
+# Chargement du modèle entraîné
+model = joblib.load('models/RandomForestRegressor_model.pkl')
+# Prédictions sur les données de test
+y_pred = model.predict(X_test)
+# Calcul des métriques d'évaluation
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+# Affichage des résultats
+print(f"Mean Squared Error: {mse}")
+print(f"R^2 Score: {r2}")
+# Sauvegarde des résultats dans un fichier json
+results = {
+    'Mean Squared Error': mse,
+    'R^2 Score': r2
+}
+with open('metrics/evaluation_results.json', 'w') as f:
+    import json         
+    json.dump(results, f, indent=4)
